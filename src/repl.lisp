a4e35b76bbb2b040e48971127ab11ff4843a20ce
Eric Lorenzana <eric.lorenzana@protonmail.ch>
2019-07-26 23:46:38 +0200

    Use flet instead of progn

--- src/repl.lisp
+++ src/repl.lisp
@@ -3,9 +3,10 @@
 (in-package :cl-monkey)
 
 (defun repl-start ()
-  (loop for lexer = (make-lexer (progn
-                                  (princ ">> ")
-                                  (read-line)))
-        do (loop for token = (lexer-next-token lexer)
-                 while (not (equalp (token-type token) +token-eof+))
-                 do (format t "~a~%" token))))
+  (flet ((repl-read ()
+           (princ ">> ")
+           (read-line)))
+    (loop for lexer = (make-lexer (repl-read))
+          do (loop for token = (lexer-next-token lexer)
+                   while (not (equalp (token-type token) +token-eof+))
+                   do (format t "~a~%" token)))))
