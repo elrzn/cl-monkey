f8ad36d57eff842a0c6ac0bc103c93cd430dd851
Eric Lorenzana <eric.lorenzana@protonmail.ch>
2019-07-26 23:45:07 +0200

    Print REPL identifier

--- src/repl.lisp
+++ src/repl.lisp
@@ -2,10 +2,10 @@
 
 (in-package :cl-monkey)
 
-(defparameter +prompt+ ">> ")
-
 (defun repl-start ()
-  (loop for lexer = (make-lexer (read-line))
+  (loop for lexer = (make-lexer (progn
+                                  (princ ">> ")
+                                  (read-line)))
         do (loop for token = (lexer-next-token lexer)
                  while (not (equalp (token-type token) +token-eof+))
                  do (format t "~a~%" token))))
